6dd086a3e0522c735cf99b75e1c994c2526324fa
fabrem <remilevesque17@gmail.com>
2021-03-12 11:16:15 -0500

    steffensen

--- steffensen.py
+++ steffensen.py
@@ -1,3 +1,6 @@
+from math import exp, cos
+
+
 def g(f, x: float):
     """First-order divided difference function.
 
@@ -6,6 +9,7 @@ def g(f, x: float):
         x(float): Point at which to evaluate g
     """
     return lambda x: f(x + f(x)) / f(x) - 1
+
 
 def steff(f, x: float):
     """Steffenson algorithm for finding roots.
@@ -17,7 +21,14 @@ def steff(f, x: float):
         f(callable): Function whose root we are searching for
         x(float): Starting value upon first call, each level n that the function recurses x is x_n
     """
-
+    print(x)
     if g(f, x)(x) != 0:
         yield x - f(x) / g(f, x)(x)  # First give x_n + 1
         yield from steff(f, x - f(x) / g(f, x)(x))  # Then give new iterator
+
+
+if __name__ == '__main__':
+    special_func = lambda x: cos(x)
+    binou = steff(special_func, 0)
+    for element in binou:
+        print(element)
